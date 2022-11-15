d87c6302d90ac0b951c28644191dc5cd23a09997
vivashka <38818762+vivashka@users.noreply.github.com>
2022-11-15 20:12:06 +0500

    asdasdasdasdasd

---
+++ Scripting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Программа напечатает "привет мир!"
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
